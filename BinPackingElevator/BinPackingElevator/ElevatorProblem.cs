731fc27d7d1257ade98918ff8bae2881de594bb0
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-05-06 14:18:44 +0200

    Create private fields

--- BinPackingElevator/BinPackingElevator/ElevatorProblem.cs
+++ BinPackingElevator/BinPackingElevator/ElevatorProblem.cs
@@ -5,7 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BinPackingElevator;
+
 public class ElevatorProblem
 {
-
+    private int _n;
+    private int _capacity;
+    private int[] _weights;
+    private int[] _elevators;
+    private int _minElevators;
+    private long[] _weightSums;
 }
