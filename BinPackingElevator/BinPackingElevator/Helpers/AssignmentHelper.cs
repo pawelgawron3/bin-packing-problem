6ecc93fa2ad82abd375d28d5d093a6c5be768872
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-05-06 15:39:08 +0200

    Create AssignmentHelper.cs

---
+++ BinPackingElevator/BinPackingElevator/Helpers/AssignmentHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace BinPackingElevator.Helpers;
+
+public static class AssignmentHelper
+{
+    public static void SaveBestAssignment(int count, List<List<int>> source, List<List<int>> destination)
+    {
+        destination.Clear();
+        for (int i = 0; i < count; i++)
+        {
+            destination.Add([.. source[i]]);
+        }
+    }
+}
