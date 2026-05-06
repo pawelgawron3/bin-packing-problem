7c5b4abfd852e570f8f9c4a42dc54916fd5ab738
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-05-06 14:24:13 +0200

    Add ctor

--- BinPackingElevator/BinPackingElevator/ElevatorProblem.cs
+++ BinPackingElevator/BinPackingElevator/ElevatorProblem.cs
@@ -14,4 +14,11 @@ public class ElevatorProblem
     private int[] _elevators;
     private int _minElevators;
     private long[] _weightSums;
+
+    public ElevatorProblem(int n, int capacity, int[] weights)
+    {
+        this._n = n;
+        this._capacity = capacity;
+        this._weights = weights;
+    }
 }
