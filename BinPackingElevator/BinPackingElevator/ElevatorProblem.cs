352c52b2ffd1b987a0c41caadbdee9da41b82f14
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-05-06 15:16:53 +0200

    Add FindMinimalElevators() method
    Also Solve() function now calls this method.

--- BinPackingElevator/BinPackingElevator/ElevatorProblem.cs
+++ BinPackingElevator/BinPackingElevator/ElevatorProblem.cs
@@ -24,5 +24,48 @@ public class ElevatorProblem
         _weightSums = new long[_n + 1];
         for (int i = _n - 1; i >= 0; i--)
             _weightSums[i] = _weightSums[i + 1] + _weights[i];
+
+        FindMinimalElevators(0, 0);
+        return _minElevators;
+    }
+
+    private void FindMinimalElevators(int personIndex, int currentElevatorsCount)
+    {
+        if (currentElevatorsCount >= _minElevators) return;
+
+        if (personIndex == _n)
+        {
+            _minElevators = currentElevatorsCount;
+            return;
+        }
+
+        long remainingWeight = _weightSums[personIndex];
+        if (currentElevatorsCount + Math.Ceiling((double)remainingWeight / _capacity) > _minElevators)
+            return;
+
+        int lastCapacityTried = -1;
+
+        for (int i = 0; i < currentElevatorsCount; i++)
+        {
+            if (_elevators[i] + _weights[personIndex] <= _capacity)
+            {
+                if (_elevators[i] == lastCapacityTried) continue;
+
+                _elevators[i] += _weights[personIndex];
+                FindMinimalElevators(personIndex + 1, currentElevatorsCount);
+                _elevators[i] -= _weights[personIndex];
+
+                lastCapacityTried = _elevators[i];
+
+                if (_elevators[i] + _weights[personIndex] == _capacity) return;
+            }
+        }
+
+        if (currentElevatorsCount + 1 < _minElevators)
+        {
+            _elevators[currentElevatorsCount] = _weights[personIndex];
+            FindMinimalElevators(personIndex + 1, currentElevatorsCount + 1);
+            _elevators[currentElevatorsCount] = 0;
+        }
     }
 }
