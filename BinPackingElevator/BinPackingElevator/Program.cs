877c2ef0381f0233154c488c6342869fc068fe1c
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-05-06 16:06:28 +0200

    Update Program.cs

--- BinPackingElevator/BinPackingElevator/Program.cs
+++ BinPackingElevator/BinPackingElevator/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinPackingElevator;
+
+// Example
+int n = 12;
+int x = 90;
+int[] weights =
+[
+    34, 66, 32, 55, 54,
+    46, 80, 82, 20, 71,
+    5, 5
+];
+
+ElevatorProblem elevatorProblem = new ElevatorProblem(n, x, weights);
+elevatorProblem.Solve();
+elevatorProblem.PrintResult();
