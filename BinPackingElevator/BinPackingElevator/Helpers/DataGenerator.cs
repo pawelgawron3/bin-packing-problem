46605c6d1f0fe61160e6b93f4a2d47d5c3e00dda
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-05-12 11:30:09 +0200

    Create DataGenerator.cs

---
+++ BinPackingElevator/BinPackingElevator/Helpers/DataGenerator.cs
@@ -0,0 +1,20 @@
+﻿namespace BinPackingElevator.Helpers;
+
+public static class DataGenerator
+{
+    public static (int n, int x, int[] weights) GenerateData(int? seed = null)
+    {
+        Random random = seed.HasValue ? new Random(seed.Value) : new Random();
+
+        int n = random.Next(1, 31);
+        int capacity = random.Next(1, 1_000_001);
+        int[] weights = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            weights[i] = random.Next(1, capacity + 1);
+        }
+
+        return (n, capacity, weights);
+    }
+}
