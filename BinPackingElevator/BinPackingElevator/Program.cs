6c32b830bd3449e2542a3ce3d3c90caf9dea6caa
pawelgawron3 <148467604+pawelgawron3@users.noreply.github.com>
2026-05-12 11:34:51 +0200

    Use DataGenerator.GenerateData() instead of hardcoded example

--- BinPackingElevator/BinPackingElevator/Program.cs
+++ BinPackingElevator/BinPackingElevator/Program.cs
@@ -1,14 +1,7 @@
 ﻿using BinPackingElevator;
+using BinPackingElevator.Helpers;
 
-// Example
-int n = 12;
-int x = 90;
-int[] weights =
-[
-    34, 66, 32, 55, 54,
-    46, 80, 82, 20, 71,
-    5, 5
-];
+var (n, x, weights) = DataGenerator.GenerateData();
 
 ElevatorProblem elevatorProblem = new ElevatorProblem(n, x, weights);
 elevatorProblem.Solve();
